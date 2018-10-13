e70fa8bd6e6f06eec3941c701739c0f483d78d5d
KailunHuang <kailunh@student.unimelb.edu.au>
2018-10-13 21:23:43 +1100

    initialize the local map

--- src/mycontroller/MyAIController.java
+++ src/mycontroller/MyAIController.java
@@ -1,12 +1,27 @@
 package mycontroller;
 
 import controller.CarController;
+import tiles.MapTile;
+import utilities.Coordinate;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.ArrayList;
 import world.Car;
 
 public class MyAIController extends CarController{
-
+	
+	HashMap<Coordinate, MapTile> map = getMap();
+	HashMap<Coordinate, Boolean> detected = new HashMap<Coordinate, Boolean>();
+	HashMap<Coordinate, Boolean> Track = new HashMap<Coordinate, Boolean>();
+	
+	
+	
 	public MyAIController(Car car) {
 		super(car);
+		for (Coordinate x : map.keySet()) {
+			detected.put(x, false);
+		}
 	}
 
 	@Override
@@ -14,5 +29,6 @@ public class MyAIController extends CarController{
 		// TODO Auto-generated method stub
 		
 	}
-
+	
+	
 }
